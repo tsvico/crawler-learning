44b2f949e2f46b5b1d18d34aebcf91a7504334fa
tsvico <tsxygwj@163.com>
2018-05-09 10:15:31 +0800

    表情包good

--- demo10.py
+++ demo10.py
@@ -1,7 +1,10 @@
 #encoding: utf-8
 import requests
+import threading
+import urllib
 from bs4 import BeautifulSoup
 PAGE_URL_LIST = []
+#表情包链接列表
 BESE_PAGE_URL = "https://www.doutula.com/photo/list/?page="
 for x in range(1,1500):
     url = BESE_PAGE_URL + str(x)
@@ -18,3 +21,16 @@ for page_url in PAGE_URL_LIST:
     # 也就是说，如果你想获取文本，可以通过.text
     # 如果你想获取图片，文件可以通过　.content
     soup = BeautifulSoup(response.content,'lxml')
+    #获取所有的标签
+    img_list = soup.find('div',class_ = 'page-content').find_all('img',class_ = 'img-responsive lazy image_dta')
+    for img in img_list:
+        title = img['alt']
+        src = img['data-original']
+        print(title)
+        print(src)
+        #获取图片名称
+        filename = title
+        #拼接路径下载
+        #path = os.path.join("image2",filename)
+        path = 'images2'+ '/' +title.strip().replace('?','')
+        urllib.urlretrieve(src,path)
