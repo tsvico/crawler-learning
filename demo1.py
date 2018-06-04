b64ffec383d3776b5ec99caec66c93bdf8e3f595
tsvico <32492304+tsvico@users.noreply.github.com>
2018-06-04 09:25:32 +0800

    Update demo1.py

--- demo1.py
+++ demo1.py
@@ -4,7 +4,8 @@ import urllib
 import urllib2
 #mm = raw_input("输入参数1：")
 value = {}#如果提交数据中需要传参，在这里填入
-header = {'User-Agent':'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.139 Safari/537.36','host':'www.baidu.com'}
+header = {'User-Agent':'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.139 Safari/537.36',
+          'host':'www.baidu.com'}
 #头中多个数据用','相隔
 url = "http://www.baidu.com/"
 data = urllib.urlencode(value)
