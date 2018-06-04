800808651e14f2c1356b0ba46fff45d84fab72ef
tsvico <32492304+tsvico@users.noreply.github.com>
2018-06-04 09:20:54 +0800

    Update demo1.py

--- demo1.py
+++ demo1.py
@@ -2,16 +2,11 @@
 #有header
 import urllib
 import urllib2
-mm = raw_input("input music :")
-value = {"input":mm,
-         "filter":"name",
-         "type":"netease",
-         "page":"1"}
-X = 'XMLHttpRequest'
-header = {'User-Agent':'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.139 Safari/537.36',
-          'X-Requested-With':X
-          }
-url = "http://host712244607.s316.pppf.com.cn/yy/index.php"
+#mm = raw_input("输入参数1：")
+value = {}#如果提交数据中需要传参，在这里填入
+header = {'User-Agent':'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.139 Safari/537.36','host':'www.baidu.com'}
+#头中多个数据用','相隔
+url = "http://www.baidu.com/"
 data = urllib.urlencode(value)
 request = urllib2.Request(url,data,header)
 #异常处理
