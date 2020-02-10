65b57c2ae94c1e7cfe5a21835846505a4d0b74e3
tsvico <tsxygwj@163.com>
2020-02-10 15:04:39 +0800

    获取公网IP地址

--- getIp138_2.py
+++ getIp138_2.py
@@ -3,9 +3,15 @@
 import requests
 from lxml import etree
 url = 'http://ip138.com/'
-html=requests.get(url,allow_redirects=False)
-text = html.text.encode(html.encoding).decode('gb2312')
+result = requests.get(url,allow_redirects=False)
+text = result.text.encode(result.encoding).decode('gb2312')
 html = etree.HTML(text)
 html_data = html.xpath('//iframe/@src')
 for i in html_data:
     print(i)
+    res = requests.get(i,allow_redirects=False)
+    dhtml = etree.HTML(res.text)
+    dhtml_data = dhtml.xpath('/html/body/p[1]')
+    # 打印提取到的结果
+    for item in dhtml_data:
+        print(item.text)
